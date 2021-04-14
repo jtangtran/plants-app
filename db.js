f03122efd2f7f2572ce5338026b48bcee90ef3dd
Jennifer Tang-Tran <phuongtangtran@gmail.com>
2021-04-14 16:15:11 -0700

    deployment

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ const devConfig = {
 const prodConfig = {
     connectionString: process.env.DATABASE_URL
 }
-
+ 
 const pool = new Pool(process.env.NODE_ENV === "production" ? prodConfig : devConfig);
 
 module.exports = pool;
