ea04f16c50944088dfda7898c1b291ccd8dbed10
jtangtran <39253059+jtangtran@users.noreply.github.com>
2021-05-05 10:27:48 -0700

    Update plant.route.js

--- api/routes/plant.route.js
+++ api/routes/plant.route.js
@@ -3,16 +3,15 @@ const plants = require("../controllers/plant.controller.js");
 
 var router = require("express").Router();
 
-router.route("/plants").post(plants.create);
+router.route("/plants")
+ .post(plants.create);
+ .get(plants.getAllPlants);
 
-router.route("/plants").get(plants.getAllPlants);
-
-router.route("/plants/:id").get(plants.getPlantById);
+router.route("/plants/:id")
+ .get(plants.getPlantById);
+ .put(plants.updatePlant);
+ .delete(plants.deletePlant);
 
 router.route("/plants/title/:title").get(plants.getPlantInfoByTitle);
- 
-router.route("/plants/:id").put(plants.updatePlant);
-
-router.route("/plants/:id").delete(plants.deletePlant);
 
 module.exports = router;
